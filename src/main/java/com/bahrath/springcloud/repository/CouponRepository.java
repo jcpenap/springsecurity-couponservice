b03cf60645a0964e0a8d9ea739b0ee36593f732b
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-29 15:58:30 -0500

    Adding Coupon Reposity

---
+++ src/main/java/com/bahrath/springcloud/repository/CouponRepository.java
@@ -0,0 +1,8 @@
+package com.bahrath.springcloud.repository;
+
+import com.bahrath.springcloud.model.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+}
