3dac20dfeaf62d38424b78046e52e9ebcedd38f3
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-29 15:57:24 -0500

    First commit

---
+++ src/test/java/com/bahrath/springcloud/CouponserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahrath.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
