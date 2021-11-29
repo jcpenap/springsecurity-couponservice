f7e5793403c8602d04b7153d642eaddd0efbf2be
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-29 15:57:55 -0500

    Adding Coupon Entity

---
+++ src/main/java/com/bahrath/springcloud/model/Coupon.java
@@ -0,0 +1,23 @@
+package com.bahrath.springcloud.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private BigDecimal discount;
+    private String expDate;
+}
