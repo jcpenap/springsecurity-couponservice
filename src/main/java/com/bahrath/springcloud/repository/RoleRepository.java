dea80f5b56bd0dbf1e8e09bcbb2613f14de2ba21
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-30 16:24:32 -0500

    Adding the repository interfaces

---
+++ src/main/java/com/bahrath/springcloud/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.bahrath.springcloud.repository;
+
+import com.bahrath.springcloud.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
