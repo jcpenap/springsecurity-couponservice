22a3aa90c80877fa7d145b835d6762ad3d04455e
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2021-11-30 16:16:14 -0500

    Creating entities and defining relationships

---
+++ src/main/java/com/bahrath/springcloud/model/User.java
@@ -0,0 +1,25 @@
+package com.bahrath.springcloud.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_ide"))
+    private Set<Role> roles;
+}
