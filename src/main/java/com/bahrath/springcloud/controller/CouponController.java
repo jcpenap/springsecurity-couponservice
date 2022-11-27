2e97cd2ca6e2f03b83a7b031eb7bed43ec05bca6
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-11-27 16:27:30 -0500

    Implement UserDetailsService and cleaning the code

--- src/main/java/com/bahrath/springcloud/controller/CouponController.java
+++ src/main/java/com/bahrath/springcloud/controller/CouponController.java
@@ -5,8 +5,6 @@ import com.bahrath.springcloud.repository.CouponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/couponapi")
 public class CouponController {
