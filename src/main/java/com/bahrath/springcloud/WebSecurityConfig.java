3767b63c35bd78a71662b2c24b38be0bf26cfe3f
Juan Carlos Pena Pena <juancarlos.pena@globant.com>
2022-11-27 16:38:45 -0500

    Adding the WebSecurityConfig file

---
+++ src/main/java/com/bahrath/springcloud/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.bahrath.springcloud;
+
+import com.bahrath.springcloud.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig implements WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
